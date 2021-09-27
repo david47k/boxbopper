f6d59fb035de48a758c766df6e5bc4c2bb7433be
David Atkinson <david47k@d47.co>
2021-09-27 13:28:30 +1000

    add a few comments

--- boxboppertool/defs.rs
+++ boxboppertool/defs.rs
@@ -2,9 +2,9 @@
 //
 // defs.rs: defaults for boxboppertool
 
-pub const DEF_MAX_MOVES: u16 = 200;
+pub const DEF_MAX_MOVES: u16 = 200;         // Our implementation currently has a hard limit of 255 total moves
 pub const DEF_MAX_DEPTH: u16 = 100;
-pub const DEF_WIDTH: usize = 5;
+pub const DEF_WIDTH: usize = 5; 
 pub const DEF_HEIGHT: usize = 5;
 pub const DEF_BOX_DENSITY: u32 = 20;
 pub const DEF_WALL_DENSITY: u32 = 20;
